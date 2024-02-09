8876f8bca920389240c5b7e447217758706c75fb
JuanPVL <juanpedrovallelema@gmail.com>
2024-02-08 22:16:32 -0600

    Funcionalidades locales completa
    Falta realizar los documentos dockerfile y .yaml para el compose

--- Tareas/Tarea2/frontend/src/Fotos.jsx
+++ Tareas/Tarea2/frontend/src/Fotos.jsx
@@ -1,22 +1,27 @@
-import { useState} from 'react'
+import { useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function Fotos() {
-  const [img, setImg] = useState(""); 
+  const [img, setImg] = useState([])
 
+  useEffect(() => {
+    fetch('http://localhost:3002/imagenes')
+    .then(response => response.json())
+    .then(data => setImg(data))
+  }, [])
 
   return (
     <>
       <h1>FOTOS</h1>
       <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <div>
-        <button>Subir Imagen</button>
+        {img.map((image,i) => (
+            <center key={i}>
+                <img src={image.imgbase64} alt='.....'/>
+                <figcaption>{image.fecha}</figcaption>
+            </center>
+        ))}
       </div>
     </>
   )
