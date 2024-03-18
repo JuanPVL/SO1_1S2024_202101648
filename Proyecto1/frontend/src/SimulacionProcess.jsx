0f79376e2dc2532aa0aa7d0ed502d18221b60761
JuanPVL <juanpedrovallelema@gmail.com>
2024-03-17 22:25:35 -0600

    py1

---
+++ Proyecto1/frontend/src/SimulacionProcess.jsx
@@ -0,0 +1,77 @@
+import {useState,useEffect} from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Legend, Tooltip } from 'chart.js';
+
+
+ChartJS.register(ArcElement,Tooltip,Legend)
+
+function SimulacionP() {
+    const [data, setData] = useState({
+        total:0,
+        used:0,
+        free:0,
+        porcUsed:0,
+        porcFree:0
+    });
+
+    const getRamData = async () => {
+        try {
+            const response = await CatMod();
+            console.log(response);
+            const datos = JSON.parse(response);
+            console.log(datos);
+            setData({
+                total: datos.totalRam,
+                used: datos.used,
+                free: datos.free,
+                porcUsed: datos.porcUsed,
+                porcFree: datos.porcNotUsed
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getRamData();
+        }, 500);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    return (
+        <div id="App">
+            <h1>RAM</h1>
+            <h2>Total RAM: {data.total} Mb</h2>
+            <h2>Percentage used: {data.porcUsed}%</h2>
+            <h2>Percentage free: {data.porcFree}%</h2>
+            <div style={{height: '30%', height: '30%', padding: '20px'}}>
+            <Doughnut
+                className='doughnut'
+                data={{
+                    labels: ['Used', 'Free'],
+                    datasets: [
+                        {
+                            data: [data.used, data.free],
+                            backgroundColor: [
+                                'rgba(255, 165, 0, 0.6)',
+                                'rgba(106, 90, 205, 0.6)',
+                            ],
+                            borderColor: [
+                                'rgba(255, 165, 0, 1)',
+                                'rgba(106, 90, 205, 1)',
+                            ],
+                            borderWidth: 1,
+                        },
+                    ],
+                }}
+            />
+            </div>
+        </div>
+    )
+}
+
+export default SimulacionP
--- Proyecto1/frontend/src/main.jsx
+++ Proyecto1/frontend/src/main.jsx
@@ -1,10 +1,84 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Route} from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <App />
+    </div>
+  );
+}
+
+function LiveCharts() {
+  return (
+    <div>
+      <Navbar />
+      <Graficas />
+    </div>
+  );
+}
+
+function Hystoric() {
+  return (
+    <div>
+      <Navbar />
+      <Historico />
+    </div>
+  );
+}
+
+function ProcessTree() {
+  return (
+    <div>
+      <Navbar />
+      <ArbolProcesos />
+    </div>
+  );
+}
+
+function Simulation() {
+  return (
+    <div>
+      <Navbar />
+      <SimulacionP />
+    </div>
+  );
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home/>,
+  },
+  {
+    path: "/livecharts",
+    element:<LiveCharts/>,
+  },
+  {
+    path: "/historico",
+    element:<Hystoric/>,
+  },
+  {
+    path: "/arbolprocesos",
+    element:<ProcessTree/>,
+  },
+  {
+    path: "/simulacionp",
+    element:<Simulation/>,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+
