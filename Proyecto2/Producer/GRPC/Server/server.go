0bfea2496bb240130852de298da316bfc08c4a81
JuanPVL <juanpedrovallelema@gmail.com>
2024-04-27 20:26:39 -0600

    P2

---
+++ Proyecto2/Producer/GRPC/Server/server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	pb "server/proto"
+
+	_ "github.com/go-sql-driver/mysql"
+	"google.golang.org/grpc"
+)
+
+var ctx = context.Background()
+var db *sql.DB
+
+type server struct {
+	pb.UnimplementedGetInfoServer
+}
+
+const (
+	port = ":3001"
+)
+
+type Data struct {
+	Name  string
+	Album string
+	Year  string
+	Rank  string
+}
+
+func mysqlConnect() {
+	dsn := "root:secret@tcp(34.30.155.164:3306)/tarea4"
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Conexión a MySQL exitosa")
+}
+
+func (s *server) ReturnInfo(ctx context.Context, in *pb.RequestId) (*pb.ReplyInfo, error) {
+	fmt.Println("Recibí de client: ", in.GetName())
+	data := Data{
+		Name:  in.GetName(),
+		Album: in.GetAlbum(),
+		Year:  in.GetYear(),
+		Rank:  in.GetRank(),
+	}
+	fmt.Println(data)
+	insertMySQL(data)
+	return &pb.ReplyInfo{Info: "Hola cliente, recibí el comentario"}, nil
+}
+
+func insertMySQL(music Data) {
+	query := "INSERT INTO music (name, album, year_release, rank_album) VALUES (?, ?, ?, ?)"
+	_, err := db.ExecContext(ctx, query, music.Name, music.Album, music.Year, music.Rank)
+	if err != nil {
+		log.Println("Error al insertar en Base de Datos:", err)
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGetInfoServer(s, &server{})
+
+	mysqlConnect()
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalln(err)
+	}
+}
